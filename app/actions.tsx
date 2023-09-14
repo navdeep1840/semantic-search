9e962a66a5d8d23eb5eec7b2b78eac5bc228c41a
Navdeep <navdeep3589@gmail.com>
2023-09-14 23:19:35 +0530

    semantic search

--- app/actions.tsx
+++ app/actions.tsx
@@ -1,48 +1,68 @@
-'use server'
+"use server";
 
-import prisma from '@/lib/prisma'
-import { openai } from '@/lib/openai'
-import { type Pokemon } from '@prisma/client'
-import { ratelimit } from '@/lib/utils'
+import prisma from "@/lib/prisma";
+import { openai } from "@/lib/openai";
+import { type Audits } from "@prisma/client";
+import { ratelimit } from "@/lib/utils";
 
 export async function searchPokedex(
   query: string
-): Promise<Array<Pokemon & { similarity: number }>> {
+): Promise<Array<Audits & { similarity: number }>> {
   try {
-    if (query.trim().length === 0) return []
+    if (query.trim().length === 0) return [];
 
-    const { success } = await ratelimit.limit('generations')
-    if (!success) throw new Error('Rate limit exceeded')
+    // const { success } = await ratelimit.limit("generations");
+    // if (!success) throw new Error("Rate limit exceeded");
 
-    const embedding = await generateEmbedding(query)
-    const vectorQuery = `[${embedding.join(',')}]`
+    const embedding = await generateEmbedding(query);
+
+    const vectorQuery = `[${embedding.join(",")}]`;
+
+    console.log("🚀 ~ file: actions.tsx:21 ~ vectorQuery:", vectorQuery);
+    // const pokemon = await prisma.$queryRaw`
+    //   SELECT
+    //     id,
+    //     "name",
+    //     1 - (embedding <=> ${vectorQuery}::vector) as similarity
+    //   FROM pokemon
+    //   where 1 - (embedding <=> ${vectorQuery}::vector) > .5
+    //   ORDER BY  similarity DESC
+    //   LIMIT 8;
+    // `;
+
     const pokemon = await prisma.$queryRaw`
       SELECT
-        id,
-        "name",
-        1 - (embedding <=> ${vectorQuery}::vector) as similarity
-      FROM pokemon
-      where 1 - (embedding <=> ${vectorQuery}::vector) > .5
-      ORDER BY  similarity DESC
-      LIMIT 8;
-    `
+     id,
+    "name",
+     1 - (embedding <=> ${vectorQuery}::vector) as similarity
+      FROM reports_data
+       where 1 - (embedding <=> ${vectorQuery}::vector) > .6
+       ORDER BY  similarity DESC
+      LIMIT 2;
+    `;
 
-    return pokemon as Array<Pokemon & { similarity: number }>
+    console.log(pokemon, `searched values`);
+
+    return pokemon as Array<Audits & { similarity: number }>;
   } catch (error) {
-    console.error(error)
-    throw error
+    console.error(error);
+    throw error;
   }
 }
 
 async function generateEmbedding(raw: string) {
   // OpenAI recommends replacing newlines with spaces for best results
-  const input = raw.replace(/\n/g, ' ')
+  const input = raw.replace(/\n/g, " ");
   const embeddingResponse = await openai.createEmbedding({
-    model: 'text-embedding-ada-002',
+    model: "text-embedding-ada-002",
     input,
-  })
+  });
 
-  const embeddingData = await embeddingResponse.json()
-  const [{ embedding }] = (embeddingData as any).data
-  return embedding
+  const embeddingData = await embeddingResponse.json();
+  console.log(
+    "🚀 ~ file: actions.tsx:60 ~ generateEmbedding ~ embeddingData:",
+    embeddingData
+  );
+  const [{ embedding }] = (embeddingData as any).data;
+  return embedding;
 }
--- components/search.tsx
+++ components/search.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import {
   Command,
@@ -6,43 +6,43 @@ import {
   CommandInput,
   CommandItem,
   CommandList,
-} from '@/components/command'
-import { type Pokemon } from '@prisma/client'
-import { useEffect, useState } from 'react'
-import { toast } from 'react-hot-toast'
-import { useDebounce } from 'use-debounce'
+} from "@/components/command";
+import { type Pokemon } from "@prisma/client";
+import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+import { useDebounce } from "use-debounce";
 
 export interface SearchProps {
   searchPokedex: (
     content: string
-  ) => Promise<Array<Pokemon & { similarity: number }>>
+  ) => Promise<Array<Pokemon & { similarity: number }>>;
 }
 
 export function Search({ searchPokedex }: SearchProps) {
-  const [query, setQuery] = useState('')
+  const [query, setQuery] = useState("");
   const [searchResults, setSearchResults] = useState<
     Array<Pokemon & { similarity?: number }>
-  >([])
-  const [debouncedQuery] = useDebounce(query, 150)
+  >([]);
+  const [debouncedQuery] = useDebounce(query, 1500);
   useEffect(() => {
-    let current = true
+    let current = true;
     if (debouncedQuery.trim().length > 0) {
       searchPokedex(debouncedQuery).then((results) => {
         if (current) {
-          setSearchResults(results)
+          setSearchResults(results);
         }
-      })
+      });
     }
     return () => {
-      current = false
-    }
-  }, [debouncedQuery, searchPokedex])
+      current = false;
+    };
+  }, [debouncedQuery, searchPokedex]);
   return (
     <div className="w-full">
       <Command label="Command Menu" shouldFilter={false} className="h-[200px]">
         <CommandInput
           id="search"
-          placeholder="Search for Pokémon"
+          placeholder="Search"
           className="focus:ring-0 sm:text-sm text-base focus:border-0 border-0 active:ring-0 active:border-0 ring-0 outline-0"
           value={query}
           onValueChange={(q) => setQuery(q)}
@@ -55,8 +55,8 @@ export function Search({ searchPokedex }: SearchProps) {
               value={pokemon.name}
               className="data-[selected='true']:bg-zinc-50  flex items-center justify-between py-3"
               onSelect={(p) => {
-                console.log(p)
-                toast.success(`You selected ${p}!`)
+                console.log(p);
+                toast.success(`You selected ${p}!`);
               }}
             >
               <div className="flex items-center space-x-4">
@@ -80,7 +80,7 @@ export function Search({ searchPokedex }: SearchProps) {
         </CommandList>
       </Command>
     </div>
-  )
+  );
 }
 
-Search.displayName = 'Search'
+Search.displayName = "Search";
